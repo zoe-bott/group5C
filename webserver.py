cc00f57c8d38ebfcfa91107e705f46d64c44a641
Georgina Wilcox <georginawilcox@gmail.com>
2017-01-11 19:45:20 +1100

    Using jinja2 for templates

--- webserver.py
+++ webserver.py
@@ -1,11 +1,18 @@
 import os
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from tornado.ncss import Server
 
 
+env = Environment(
+    loader=FileSystemLoader('templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
 
 def index_handler(request):
-  request.write("Hello world")
+  template = env.get_template('index.html')
+  response = template.render()
+  request.write(response)
 
 
 PORT = int(os.environ.get("PORT", "8888"))
